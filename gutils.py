2bb1a3e0dc9fa0d161bf417686a467322c5dbae3
Leo Wang <54620490+wyy-code@users.noreply.github.com>
2024-01-29 16:14:44 +0800

    Update gutils.py

--- gutils.py
+++ gutils.py
@@ -6,82 +6,38 @@ import scipy.sparse as sp
 import math
 import torch
 import tensorflow as tf
+import tensorly
 
 
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    
-def simple_randomized_torch_svd(B, k):
-    _, n = B.size()
-    rand_matrix = torch.rand((n,k), dtype=torch.float64).to(device)   
-    Q, _ = torch.qr(B @ rand_matrix)                                # qr decomposition
-    Q.to(device)
-    smaller_matrix = (Q.transpose(0, 1) @ B).to(device)
-    U_hat, s, V = torch.svd(smaller_matrix, True)                   # matrix decompostion
-    U_hat.to(device)
-    U = (Q @ U_hat)
-
-    return U @ (s.pow(0.5).diag()), V @ (s.pow(0.5).diag())     # for link prediction
-
-
-def computeP4svd(prob, hi, threshold=1e-5, niter=8,alpha=0.5):
-    # hi = torch.tensor(identity,dtype=torch.float)
-    # prob = torch.tensor(prob,dtype=torch.float)
+def computeP4svd(prob, threshold=1e-5, niter=8,alpha=0.5):
+    hi = tf.eye(prob.shape[0], dtype="float32")
     prx_mat = hi * alpha
     print("begin SVD iter...")
     for i in range(niter):
-        # hi = (prob @ hi) * (1 - alpha)
-        hi = (prob@hi) * (1 - alpha)
+        hi = tf.sparse.sparse_dense_matmul(prob, hi) * (1 - alpha)
         prx_mat += hi * alpha
         print(f"before SVD iter{i}")
     prx_mat /= threshold
-    prx_mat = prx_mat.todense()
     prx_mat[prx_mat < 1] = 1.
     # prx_mat_log = torch.from_numpy(prx_mat)
     prx_mat = torch.from_numpy(prx_mat)
     prx_mat_log = prx_mat.log().to_sparse().requires_grad_(False)
     # U, V = simple_randomized_torch_svd(prx_mat_log, 128)
     print("begin torch SVD...")
-    U, sigma, V = torch.svd_lowrank(prx_mat_log, q=128)
+    U, sigma, _ = tensorly.truncated_svd(prx_mat_log, n_eigenvecs=512)
+    # U, sigma, V = torch.svd_lowrank(prx_mat_log, q=128)
     U = U @ (sigma.pow(0.5).diag())
 
     return U.numpy()
     
 
+def convert_scipy_to_sparsetensor(matrix):
+    data = matrix.data.astype('float32')
+    row = matrix.nonzero()[0]
+    col = matrix.nonzero()[1]
+    indices = np.vstack((row,col)).T
 
-
-def normalize_adj(adj):
-    adj = sp.coo_matrix(adj)
-    rowsum = np.array(adj.sum(1))
-    d_inv_sqrt = np.power(rowsum, -1).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    return d_mat_inv_sqrt.dot(adj)
-
-def get_adj_p_matrix(triples, entity, rel):
-    if triples[0][0]%2 == 0:
-        ent_size = int(max(entity)/2 + 1)
-        adj_p = sp.lil_matrix((ent_size, ent_size))
-        for h, r, t in triples:    
-            # adj_matrix[h, t] = 1;
-            # adj_matrix[t, h] = 1;
-            adj_p[h/2, t/2] = 1;
-            adj_p[t/2, h/2] = 1;
-    else:
-        ent_size = int((max(entity)-1)/2 + 1)
-        adj_p = sp.lil_matrix((ent_size, ent_size))
-        for h, r, t in triples:    
-            # adj_matrix[h, t] = 1;
-            # adj_matrix[t, h] = 1;
-            adj_p[(h-1)/2, (t-1)/2] = 1;
-            adj_p[(t-1)/2, (h-1)/2] = 1;
-    #adj_matrix = sp.lil_matrix((ent_size, ent_size))
-
-    adj_p = normalize_adj(adj_p)
-    return adj_p
-
-
+    return tf.SparseTensor(indices=indices, values=data, dense_shape=matrix.shape)
 
 def compute_P(adj_1, adj_2, sims, alpha=0.5, k=5):
     # get original KG1 to KG2 matrix
@@ -93,16 +49,18 @@ def compute_P(adj_1, adj_2, sims, alpha=0.5, k=5):
     # get topK and normalize
     cos_12 = sparse_top_k(cos_12,k)
     cos_21 = sparse_top_k(cos_21,k)
+    adj_1 = convert_scipy_to_sparsetensor(adj_1)
+    adj_2 = convert_scipy_to_sparsetensor(adj_2)
 
-    # stack 4 parts
-    adj_1 = np.concatenate((adj_1*alpha,cos_12*(1-alpha)),axis=1)
-    adj_2 = np.concatenate((cos_21*(1-alpha), adj_2*alpha),axis=1)
-    adj_1 = sp.csr_matrix(adj_1)
-    adj_2 = sp.csr_matrix(adj_2)
-    # P = np.concatenate((adj_1,adj_2),axis=0)
-    P = sp.vstack((adj_1,adj_2))
+    cos_12 = tf.sparse.map_values(lambda x: x * (1-alpha), cos_12)
+    adj_1 = tf.sparse.map_values(lambda x: x * alpha, adj_1)
+    cos_21 = tf.sparse.map_values(lambda x: x * (1 - alpha), cos_21)
+    adj_2 = tf.sparse.map_values(lambda x: x * (1 - alpha), adj_2)
 
-    return P
+    adj_1 = tf.sparse.concat(axis=-1, sp_inputs=[adj_1, cos_12])
+    adj_2 = tf.sparse.concat(axis=-1, sp_inputs=[cos_21, adj_2])
+
+    return tf.sparse.concat(axis=0, sp_inputs=[adj_1, adj_2])
 
 def sparse_top_k(sim, k):
    # numpy matrix convert to tensorflow
@@ -124,12 +82,9 @@ def sparse_top_k(sim, k):
    result = tf.where(mask, sim_tensor, tf.zeros_like(sim_tensor))
 
    # convert to sparse matrix
-   # sparse_matrix = tf.sparse.from_dense(result)
+   sparse_matrix = tf.sparse.from_dense(result)
 
-   return result.numpy()
-
-
-
+   return sparse_matrix
 
 
 def reshape_P(P):
@@ -188,18 +143,3 @@ def get_dev_sims(test_pair, sims):
     sims = tf.gather(indices=test_pair[:,1],params=sims, axis=1)
     return sims
 
-def get_dev_torch_sims(test_pair, sims):
-    row = torch.tensor(test_pair[:,0])
-    col = torch.tensor(test_pair[:,1])
-    sims_row = tf.gather(indices=test_pair[:,0],params=sims, axis=0)
-    dev_sims = tf.gather(indices=test_pair[:,1],params=sims_row, axis=1)
-    return dev_sims
-
-
-# if __name__ == '__main__':
-#     sim=np.random.rand(4,5)
-#     print(sim)
-#     k=2
-#     sparse_mat=sparse_top_k(sim,k)
-#
-#     print(sparse_mat)
