3298b5dd6c33b9093628d1f29b32ab481cb9b66a
Leo Wang <54620490+wyy-code@users.noreply.github.com>
2024-01-29 12:27:11 +0800

    Update gutils.py

--- gutils.py
+++ gutils.py
@@ -1,24 +1,16 @@
 import numpy as np
 import sklearn
 from sklearn import decomposition
-import torch
 from scipy import linalg
 import scipy.sparse as sp
 import math
 import torch
 import tensorflow as tf
-import torch.nn as nn
-from torch.nn.parameter import Parameter
-import heapq
-import sys
-import torch.nn.functional as F
+
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-def convert_to_64bit_indices(A):
-    A.indptr = np.array(A.indptr, copy=False, dtype=np.int64)
-    A.indices = np.array(A.indices, copy=False, dtype=np.int64)
-    return A
+
     
 def simple_randomized_torch_svd(B, k):
     _, n = B.size()
@@ -56,70 +48,7 @@ def computeP4svd(prob, hi, threshold=1e-5, niter=8,alpha=0.5):
 
     return U.numpy()
     
-def computeP4svd_topk(prob, hi, threshold=1e-5, niter=8,alpha=0.5):
-    # hi = torch.tensor(identity,dtype=torch.float)
-    # prob = torch.tensor(prob,dtype=torch.float)
-    prx_mat = hi * alpha
-    prx_mat = convert_to_64bit_indices(prx_mat)
-    print("begin SVD iter...")
-    for i in range(niter):
-        # hi = (prob @ hi) * (1 - alpha)
-        hi = (prob@hi) * (1 - alpha)
-        prx_mat += hi * alpha
-        print(f"before SVD iter{i}")
-    prx_mat /= threshold
-    prx_mat = prx_mat.todense()
-    prx_mat[prx_mat < 1] = 1.
-    #prx_mat[prx_mat < threshold] = 0.
-    # prx_mat = torch.from_numpy(prx_mat)
-    prx_mat -= 1.
-    prx_mat = sp.coo_matrix(prx_mat)
-    prx_mat = prx_mat.log1p()
-    values = prx_mat.data
-    indices = np.vstack((prx_mat.row, prx_mat.col))   
-    i = torch.LongTensor(indices)
-    v = torch.FloatTensor(values)
-    shape = prx_mat.shape    
-    prx_mat = torch.sparse.FloatTensor(i, v, torch.Size(shape))
-    #prx_mat_log = prx_mat.log().to_sparse().requires_grad_(False)
-    # U, V = simple_randomized_torch_svd(prx_mat_log, 128)
-    print("begin torch SVD...")
-    U, sigma, V = torch.svd_lowrank(prx_mat, q=128)
-    U = U @ (sigma.pow(0.5).diag())
 
-    return U.numpy()
-    
-def computeP4svd_sparse(ppr, threshold=1e-5, niter=8,alpha=0.5):
-    prx_mat = ppr/threshold
-    prx_mat = prx_mat.todense()
-    prx_mat[prx_mat < 1] = 1.
-    # prx_mat_log = torch.from_numpy(prx_mat)
-    # prx_mat = torch.from_numpy(prx_mat)
-    prx_mat -= 1.
-    prx_mat = sp.coo_matrix(prx_mat)
-    prx_mat = prx_mat.log1p()
-    values = prx_mat.data
-    indices = np.vstack((prx_mat.row, prx_mat.col))   
-    i = torch.LongTensor(indices)
-    v = torch.FloatTensor(values)
-    shape = prx_mat.shape    
-    prx_mat = torch.sparse.FloatTensor(i, v, torch.Size(shape))
-    print("begin torch SVD...")
-    U, sigma, V = torch.svd_lowrank(prx_mat, q=128)
-    U = U @ (sigma.pow(0.5).diag())
-
-    return U.numpy()
-
-
-def load_triples(file_name):
-    triples = []
-    entity = set()
-    rel = set([0])
-    for line in open(file_name,'r'):
-        head,r,tail = [int(item) for item in line.split()]
-        entity.add(head); entity.add(tail); rel.add(r+1)
-        triples.append((head,r+1,tail))
-    return entity,rel,triples
 
 
 def normalize_adj(adj):
@@ -152,57 +81,18 @@ def get_adj_p_matrix(triples, entity, rel):
     adj_p = normalize_adj(adj_p)
     return adj_p
 
-def compute_adj_p(lang):
-    entity1, rel1, triples1 = load_triples(lang + 'triples_1')
-    entity2, rel2, triples2 = load_triples(lang + 'triples_2')
-    adj_p_1 = get_adj_p_matrix(triples1,entity1,rel1)
-    adj_p_2 = get_adj_p_matrix(triples2,entity2,rel2)
-    return adj_p_1,adj_p_2
-
-def get_cos_similar_matrix(v1, v2):
-    num = np.dot(v1, np.array(v2).T)  # dot
-    denom = np.linalg.norm(v1, axis=1).reshape(-1, 1) * np.linalg.norm(v2, axis=1)
-    res = num / denom
-    res[np.isneginf(res)] = 0
-    return 0.5 + 0.5 * res
-
-def get_k_max(array,k):
-    _k_sort = np.argpartition(array,-k)[-k:]
-    return array[_k_sort]
-
-def top_k_matrix(ori_matrix, k):
-    for i in range(len(ori_matrix)):
-        threshold = np.min(get_k_max(ori_matrix[i],k))
-        if threshold<0.5:
-            # print("The threshold is:",threshold)
-            threshold = np.min(get_k_max(ori_matrix[i],k+1))
-        ori_matrix[i][ori_matrix[i]<threshold] = 0
-
-    ori_matrix = normalize_adj(ori_matrix)
-    # print(ori_matrix.todense())
-        
-    return ori_matrix.todense()
-    
-def top_k_matrix_sparse(ori_matrix, k):
-    for i in range(len(ori_matrix)):
-        threshold = torch.min(ori_matrix[i].topk(k, sorted=True)[0])
-        ori_matrix[i][ori_matrix[i]<threshold] = 0
-    ori_matrix = torch.nn.functional.normalize(ori_matrix, p=2, dim=1)
-    # print(ori_matrix.todense())
-        
-    return ori_matrix
 
 
-def compute_P(adj_1, adj_2, E1, E2, alpha=0.5, k=5):
+def compute_P(adj_1, adj_2, sims, alpha=0.5, k=5):
     # get original KG1 to KG2 matrix
     # cos_12 = get_cos_similar_matrix(E1, E2)
 
-    cos_12 = np.maximum(0,sklearn.metrics.pairwise.cosine_similarity(E1, E2))
+    cos_12 = sims
     cos_21 = cos_12.T
 
     # get topK and normalize
-    cos_12 = top_k_matrix(cos_12,k)
-    cos_21 = top_k_matrix(cos_21,k)
+    cos_12 = sparse_top_k(cos_12,k)
+    cos_21 = sparse_top_k(cos_21,k)
 
     # stack 4 parts
     adj_1 = np.concatenate((adj_1*alpha,cos_12*(1-alpha)),axis=1)
@@ -213,6 +103,34 @@ def compute_P(adj_1, adj_2, E1, E2, alpha=0.5, k=5):
     P = sp.vstack((adj_1,adj_2))
 
     return P
+
+def sparse_top_k(sim, k):
+   # numpy matrix convert to tensorflow
+   sim_tensor = tf.constant(sim, dtype=tf.float32)
+
+   # find the max k value and indices
+   _, indices = tf.math.top_k(sim_tensor, k)
+
+   # initial bool mask as False
+   mask = tf.fill(tf.shape(sim_tensor), False)
+
+   # update bool mask
+   row_indices = tf.range(tf.shape(sim_tensor)[0])[:, tf.newaxis]
+   full_indices = tf.reshape(tf.concat([tf.repeat(row_indices, k, axis=1), indices], axis=-1), [-1, 2])
+   updates = tf.fill([k * tf.shape(sim_tensor)[0]], True)
+   mask = tf.tensor_scatter_nd_update(mask, full_indices, updates)
+
+   # utilize bool mask to the original matrix
+   result = tf.where(mask, sim_tensor, tf.zeros_like(sim_tensor))
+
+   # convert to sparse matrix
+   # sparse_matrix = tf.sparse.from_dense(result)
+
+   return result.numpy()
+
+
+
+
 
 def reshape_P(P):
     p1 = P[:int(P.shape[0]/2),]
@@ -235,19 +153,6 @@ def refina(a1, a2, M, k=8):
         print("Refina in iter {}".format(i))
     return M
 
-def refina_sparse(a1, a2, M, k=8):
-    a1, a2 = torch.tensor(a1, dtype=torch.float32).to_sparse().requires_grad_(False), torch.tensor(a2,dtype=torch.float32).to_sparse().requires_grad_(False)
-    print(a1.dtype,a2.dtype,M.dtype)
-    # M = torch.tensor(M, dtype=torch.float32)
-    M = top_k_matrix_sparse(M, 20)
-    for i in range(k):
-        M = torch.mul(M.requires_grad_(False), torch.sparse.mm(torch.sparse.mm(a1,M).to_sparse(),a2).to_dense())
-        M = M + 1e-5
-        M = torch.nn.functional.normalize(M, p=2, dim=1)
-        M = torch.nn.functional.normalize(M, p=2, dim=0)
-        M = top_k_matrix_sparse(M, 64)
-        print("Refina in iter {}".format(i))
-    return M
 
 def remake_adj_1(pair, adj):
     head = []
@@ -289,3 +194,12 @@ def get_dev_torch_sims(test_pair, sims):
     sims_row = tf.gather(indices=test_pair[:,0],params=sims, axis=0)
     dev_sims = tf.gather(indices=test_pair[:,1],params=sims_row, axis=1)
     return dev_sims
+
+
+# if __name__ == '__main__':
+#     sim=np.random.rand(4,5)
+#     print(sim)
+#     k=2
+#     sparse_mat=sparse_top_k(sim,k)
+#
+#     print(sparse_mat)
